c235e0e6496e5e8ef389e57644972fde6f45ab8d
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-19 23:12:25 +0400

    feat: add DirectoryAlias enum

---
+++ src/Directories/DirectoryAlias.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zorachka\Framework\Directories;
+
+use Webmozart\Assert\Assert;
+
+final class DirectoryAlias
+{
+    public const ROOT = '@root';
+    public const PUBLIC = '@public';
+}
